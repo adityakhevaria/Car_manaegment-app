70063a8bdc62f38544df36ee4fd359543f4fb23e
Aditya Khevaria <adityakhevaria@gmail.com>
2024-11-14 17:58:23 +0530

    Resolved new conflicts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,23 @@
-// .eslintrc.js
 module.exports = {
+  parser: "@typescript-eslint/parser", // Use TypeScript parser
+  parserOptions: {
+    ecmaVersion: 2020, // Modern ECMAScript support
+    sourceType: "module", // Enable ECMAScript modules
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {}, // This allows eslint to resolve TypeScript modules
+    },
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
+  ],
   rules: {
-    "@typescript-eslint/no-unused-vars": "warn", // Change "error" to "warn"
-    "@typescript-eslint/no-explicit-any": "off", // Disable if explicit "any" is needed
+    // Add or adjust rules here
   },
 };
