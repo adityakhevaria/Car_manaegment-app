a2323684b94b9dd6cf59c9cb9f69593a26aecf54
Aditya Khevaria <adityakhevaria@gmail.com>
2024-11-14 15:00:14 +0530

    Resolved new conflicts

--- app/components/SessionProvider.tsx
+++ app/components/SessionProvider.tsx
@@ -2,10 +2,11 @@
 
 import { SessionProvider as Provider } from 'next-auth/react'
 import { useEffect, useState } from 'react'
+import { Session } from 'next-auth' // Import the correct Session type
 
 type Props = {
   children: React.ReactNode
-  session: any
+  session: Session | null // Use the correct type for the session
 }
 
 export function SessionProvider({ children, session }: Props) {
